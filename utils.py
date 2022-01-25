fbf86a31839aee329fe24b215b12cded2ddd687c
Kevin Phua <kevin.phua@rapidz.io>
2022-01-25 11:44:30 +0800

    Completed sendhex

--- utils.py
+++ utils.py
@@ -26,14 +26,11 @@ def send_pulse(web3, private_key, sender_account, receiver_address, value, f):
 def send_hex(web3, private_key, sender_account, receiver_address, value, f):
 	contract_address = "0x2b591e99afE9f32eAA6214f7B7629768c40Eeb39"
 
-	with open("hex_contract_abi.json") as f:
-		info_json = json.load(f)
+	with open("hex_contract_abi.json") as json_file:
+		info_json = json.load(json_file)
 	token_abi = info_json
 	contract = web3.eth.contract(contract_address, abi=token_abi)
 
-	print(f"My HEX balance (before): {contract.functions.balanceOf(sender_account.address).call()}\n")
-	print(f"Receiver HEX balance (before): {contract.functions.balanceOf(receiver_address).call()}\n")
-	
 	raw_tx = {
 	    "chainId": CHAIN_ID,   
 	    "from": sender_account.address,
@@ -46,8 +43,6 @@ def send_hex(web3, private_key, sender_account, receiver_address, value, f):
 	}
 	signed_tx = web3.eth.account.signTransaction(raw_tx, private_key)
 	tx_hash = web3.eth.sendRawTransaction(signed_tx.rawTransaction)
-	
-	print("Tx Hash: " + web3.toHex(tx_hash) + "\n")
-
-	print(f"My HEX balance (after): {contract.functions.balanceOf(sender_account.address).call()}\n")
-	print(f"Receiver HEX balance (after): {contract.functions.balanceOf(receiver_address).call()}\n")
+	log = "Sending " + str(value) + " Hearts to " + receiver_address + ", Tx Hash: " + web3.toHex(tx_hash) + "\n"
+	print(log)
+	f.write(log)
